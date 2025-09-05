1a4a4a51e3f58992ca8f9b687eae9e36a8b7418e
ethioska <ethioska@gmail.com>
2025-09-05 11:13:16 +0300

    Create tno.js

---
+++ tno.js
@@ -0,0 +1,8 @@
+// Replace with actual TON wallet SDK / API
+async function sendTON(userId, amount) {
+    console.log(`Sending ${amount} TON to user ${userId}`);
+    // TODO: Integrate Telegram TON Wallet API here
+    return true;
+}
+
+module.exports = { sendTON };
