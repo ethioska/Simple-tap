0d2cea64e9e7f9ef42e557b1578477aa84f7a7d4
ethioska <ethioska@gmail.com>
2025-09-05 11:55:54 +0300

    Delete ton.js

--- ton.js
+++
@@ -1,8 +0,0 @@
-// Replace with actual TON wallet SDK / API
-async function sendTON(userId, amount) {
-    console.log(`Sending ${amount} TON to user ${userId}`);
-    // TODO: Integrate Telegram TON Wallet API here
-    return true;
-}
-
-module.exports = { sendTON };
